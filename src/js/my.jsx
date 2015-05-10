41d62df9faa9470cbc0bd8b0256a9eaf5ae8cc5f
RobertMaloney <robertjamesmaloney@gmail.com>
2015-05-10 16:50:21 -0700

    Panel class, harcoded urls

--- src/js/my.jsx
+++ src/js/my.jsx
@@ -1,14 +1,17 @@
-var cygwin = "C:\\cygwin64\\bin\\mintty.exe";
+var apps = ["C:\\cygwin64\\bin\\mintty.exe",
+            "C:\\Users\\Rob\\AppData\\Local\\atom\\app-0.198.0\\atom.exe"];
 
 var shell = require('shell');
 
 var Runnable = React.createClass({
   handleClick: function() {
-    shell.openExternal(cygwin);
+    shell.openExternal(apps[this.props.urlid]);
   },
   render: function() {
     var style = {
       position: 'absolute',
+      top: 10,
+      left: 10 + 45*this.props.urlid,
       width: 40,
       height: 40,
       backgroundColor: 'black'
@@ -20,4 +23,15 @@ var Runnable = React.createClass({
   }
 });
 
-React.render(<Runnable />, document.getElementById('main'));
+var Panel = React.createClass({
+  render: function () {
+    var runnables = [];
+    runnables.push(Runnable({urlid:0}));
+    runnables.push(Runnable({urlid:1}));
+    return (
+      <div>{runnables}</div>
+    )
+  }
+});
+
+React.render(<Panel />, document.getElementById('main'));
