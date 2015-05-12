a5b4d7cbfd1be4a15b951cf26340141fd9be826e
RobertMaloney <robertjamesmaloney@gmail.com>
2015-05-12 12:56:23 -0700

    Moved program to tray, basic icon

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var Menu = require('menu');
+var Tray = require('tray');
+var NativeImage = require('native-image');
 
 // Report crashes to our server.
 require('crash-reporter').start();
 
 var mainWindow = null;
+var mainTray = null;
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin')
@@ -14,7 +18,12 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   var myScreen = require('screen');
   var size = myScreen.getPrimaryDisplay().workAreaSize;
-  mainWindow = new BrowserWindow({width: size.width, height: size.height/2, frame: false});
+  mainWindow = new BrowserWindow({
+    width: size.width,
+    height: size.height/2,
+    frame: false,
+    "skip-taskbar": true
+  });
 
   mainWindow.loadUrl('file://' + __dirname + '/main.html');
   mainWindow.show();
@@ -22,4 +31,12 @@ app.on('ready', function() {
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
+  var icon = NativeImage.createFromPath(__dirname.slice(2) + '/css/tray.png');
+  mainTray = new Tray(icon);
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Quit', type: 'normal', click: function() {mainWindow.close();} }
+  ]);
+  mainTray.setToolTip('This is my application.');
+  mainTray.setContextMenu(contextMenu);
 });
